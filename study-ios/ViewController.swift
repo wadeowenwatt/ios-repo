085dcd80047a6b97ac31005f92c842d2c2780499
linhtn1 <wadeowenwatt120801@gmail.com>
2024-04-08 16:15:23 +0700

    done animation signIn form

--- study-ios/ViewController.swift
+++ study-ios/ViewController.swift
@@ -10,47 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var signInView: UIView!
-    @IBOutlet var heightConstraint: NSLayoutConstraint!
-    @IBOutlet var topConstraint: NSLayoutConstraint!
-    
-    @IBOutlet var labelSignIn: UILabel!
-    
     @IBOutlet var formView: UIView!
-    @IBOutlet var labelEmail: UILabel!
-    @IBOutlet var labelPassword: UILabel!
-    @IBOutlet var signInBtn: UIButton!
-    var isHiddingUI = true
+    @IBOutlet var heightSignInView: NSLayoutConstraint!
+    @IBOutlet var heightFormView: NSLayoutConstraint!
+    var isHiddingUI = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Setup tapGestureRecognizer
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-        signInView.addGestureRecognizer(tapGestureRecognizer)
         
         // Draw signInView
         signInView.layer.cornerRadius = 5
-        labelSignIn.textColor = .red
-        
-        // Draw form view
-        labelEmail.textColor = .systemPink
-        labelPassword.textColor = .systemPink
-        signInBtn.layer.cornerRadius = 8
+        formView.layer.cornerRadius = 3
+
     }
  
-    @objc func viewTapped() {
+    @IBAction func onTap(_ sender: Any) {
         isHiddingUI = !isHiddingUI
         if (isHiddingUI) {
-            UIView.animate(withDuration: 3.0) {
-                self.heightConstraint.constant = 80
-                self.topConstraint.constant = 310
-                self.formView.isHidden = self.isHiddingUI
-            }
+            heightSignInView.constant = 80
+            heightFormView.constant = 0
         } else {
-            UIView.animate(withDuration: 3.0) {
-                self.heightConstraint.constant = 60
-                self.topConstraint.constant = 220
-                self.formView.isHidden = self.isHiddingUI
-            }
+            heightSignInView.constant = 60
+            heightFormView.constant = 200
+        }
+        triggerAnimation()
+    }
+    
+    func triggerAnimation() {
+        UIView.animate(withDuration: 1) {
+            self.view.layoutIfNeeded()
         }
     }
     
